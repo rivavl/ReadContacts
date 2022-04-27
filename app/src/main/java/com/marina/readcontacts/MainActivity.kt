a7267a4e77996182e75c58113c030965da706771
rivavl <mvkalinenkova@yandex.ru>
2022-04-27 23:32:49 +0300

    231. Получение разрешения от пользователя

--- app/src/main/java/com/marina/readcontacts/MainActivity.kt
+++ app/src/main/java/com/marina/readcontacts/MainActivity.kt
@@ -21,8 +21,33 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("SSSSSS", "Permission denied")
+            requestPermission()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("SSSSSS", "Permission Denied")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
     }
 
     private fun requestContacts() {
@@ -49,5 +74,9 @@ class MainActivity : AppCompatActivity() {
             }
             cursor?.close()
         }
+    }
+
+    companion object {
+        private const val READ_CONTACTS_RC = 100
     }
 }
